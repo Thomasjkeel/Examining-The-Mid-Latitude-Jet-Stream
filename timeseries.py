3b51062914df97ed8b720fbdd93619cb2a17ada0
Thomasjkeel <thomasjames.keel@googlemail.com>
2017-08-30 16:39:15 +0100

    time series function

--- timeseries.py
+++ timeseries.py
@@ -1,1 +1,24 @@
+def time_series_maker(vwnd_file, uwnd_file, startdate, enddate, lon, lat, variable, myWindow=24, running_mean=False):
+    try:
+        df = wind_direction_df(vwnd_file=vwnd_file, uwnd_file=uwnd_file, startdate=startdate, enddate=enddate,
+         level=level, lat=lat, lon=lon, s=False, ret=True)
+    except:
+        raise KeyboardInterrupt, 'df maker failed'
 
+    if running_mean == True:
+        try:
+            fig = df[variable].plot()
+            mw = df[variable].rolling(window=myWindow,center=True).mean()
+            mw.plot(style='-r', label = "{0}hr Moving Window".format(myWindow))
+            return fig
+        except:
+            raise KeyboardInterrupt, 'enter valid variable'
+
+    else:
+        try:
+            fig = df[variable].plot()
+            return fig
+        except:
+            raise KeyboardInterrupt, 'enter valid variable'
+
+    
